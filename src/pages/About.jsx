720916e67e3f6bbf603cade9d369edbf342c571a
angelostori <angelostori13@gmail.com>
2025-11-16 18:05:42 +0100

    add Home and About pages

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,9 +1,12 @@
 export default function About() {
-
-
     return (
-        <>
-
-        </>
-    )
+        <div className="jumbo">
+            <div className="container">
+                <h1>Chi siamo</h1>
+                <p>
+                    Siamo un e-commerce di esempio costruito durante un esercizio React.
+                </p>
+            </div>
+        </div>
+    );
 }
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,10 @@
 export default function Home() {
-
-
     return (
-        <>
-
-        </>
+        <div className="jumbo">
+            <div className="container">
+                <h1>Benvenuto nel nostro e-commerce!</h1>
+                <p>Esplora i nostri prodotti</p>
+            </div>
+        </div>
     )
 }
