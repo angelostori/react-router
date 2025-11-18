a8b94a160a2819bcf8754157a714c10059ffd483
angelostori <angelostori13@gmail.com>
2025-11-18 09:57:42 +0100

    add a loader in the products page

--- src/pages/Products.jsx
+++ src/pages/Products.jsx
@@ -1,15 +1,19 @@
 import { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
+import { DotWave } from 'ldrs/react'
+import 'ldrs/react/DotWave.css'
 
 
 export default function Products() {
     const [products, setProducts] = useState([]);
+    const [loading, setLoading] = useState(false)
 
     useEffect(() => {
         fetch("https://fakestoreapi.com/products")
             .then((res) => res.json())
             .then((data) => {
                 setProducts(data);
+                setLoading(true);
             });
     }, []);
 
@@ -18,8 +22,7 @@ export default function Products() {
         <div className="jumbo prods">
             <div className="container">
                 <h1 className="mb-4">I nostri Prodotti</h1>
-
-                <div className="row">
+                {loading ? <div className="row">
                     {products.map((prod) => (
                         <div key={prod.id} className="col-12 col-md-4 col-lg-3 mb-4">
                             <div className="card h-100">
@@ -39,7 +42,12 @@ export default function Products() {
                             </div>
                         </div>
                     ))}
-                </div>
+                </div> : <DotWave
+                    size="47"
+                    speed="1"
+                    color="white"
+                />}
+
             </div>
 
         </div>
